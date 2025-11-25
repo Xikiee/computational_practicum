88d3b45f59b0c5030969fadedd7a46c3a5b0cce1
oliver <ollohr@tudelft.nl>
2025-11-25 23:50:27 +0100

    changes made to discretization.py

--- q_2/assignments/assignment_2/discretization.py
+++ q_2/assignments/assignment_2/discretization.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy as sp
-
+import sympy 
 
 def define_A_DF(nx:int, fo:float)->np.ndarray:
 
@@ -14,19 +14,22 @@ def define_A_DF(nx:int, fo:float)->np.ndarray:
         A_DF (np.ndarray): matrix of ...
         
     """
-    x = np.linspace(0,np.pi*3)
     matrix_size = nx -2
 
-    A_DF = np.eye(matrix_size) * (-2)
-    A_DF +=np.diag(np.ones(matrix_size-1), k=+1)
-    A_DF += np.diag(np.ones(matrix_size-1), k=-1)
-    A_DF *= fo
+    # A_DF = np.eye(matrix_size) * (-2)
+    # A_DF +=np.diag(np.ones(matrix_size-1), k=+1)
+    # A_DF += np.diag(np.ones(matrix_size-1), k=-1)
+    # A_DF *= fo
 
-    
+    A_DF = np.eye(matrix_size)* (-1/fo +2)
+    A_DF += np.diag(np.ones(matrix_size-1), k=+1)
+    A_DF += np.diag(np.ones(matrix_size-1), k=-1)
+    A_DF *= fo   
+
     return A_DF
 
-
-print(define_A_DF(10,1))
+# fo = sympy.symbols('fo')
+# print(define_A_DF(10,fo))
 
 def define_B_DF(nx: int, lbc:float, rbc:float)->np.ndarray:
     """
@@ -40,6 +43,8 @@ def define_B_DF(nx: int, lbc:float, rbc:float)->np.ndarray:
     Returns: 
         b_DF (np.ndarray): matrix of ...
     """
-
+    matrix_size = nx - 2
+    b = np.zeros(nx)
+    b[0] += lbc
+    b[-1] += rbc
     np.zeros()
-
